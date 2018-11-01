016a8fa3db01d33336469b5a2eb33f63ffd4db3a
James Chun <jchun00@stuy.edu>
2018-11-01 11:01:02 -0400

    huh

---
+++ NoNullArrayList.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+public class NoNullArrayList<T> extends ArrayList<T>{
+  public T value;
+  public NoNullArrayList(){
+    
+  }
+  public NoNullArrayList(T start){
+
+  }
+  public T getValue(){
+    return value;
+    }
+  }

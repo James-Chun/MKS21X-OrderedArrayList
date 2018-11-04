d055fbaf8632bef868c96cfb4f0a177ab910f039
James-Chun <jchun00@stuy.edu>
2018-11-04 14:45:37 -0500

    finished OrderedList

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -1,12 +1,17 @@
 import java.util.ArrayList;
 public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T> {
+  //default constructor
   public OrderedArrayList() {
     super();
   }
+
+  //constructor with starting size
   public OrderedArrayList(int initialCapacity){
     super(initialCapacity);
   }
-  public int findNewIndex(T value){
+
+  //helper method to find new sorted index
+  private int findNewIndex(T value){
     int newIndex = 0;
     for (int i = 0; i < size(); i ++) {
       if(value.compareTo(get(i))>0 ){
@@ -15,11 +20,23 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
     }
     return newIndex;
   }
+
+  //overridden add()
   public boolean add(T value){
     super.add(findNewIndex(value),value);
     return true;
   }
+
+  //overridden add(index,value)
   public void add(int index, T value){
     super.add(findNewIndex(value),value);
+  }
+
+  //overridden set()
+  public T set(int index,T value){
+    T temp = super.get(index);
+    super.remove(index);
+    super.add(findNewIndex(value),value);
+    return temp;
   }
 }
